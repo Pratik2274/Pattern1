f781e17a5aff87e46103bdfa48eebb5c366a1d8b
student14 <student14@apples-macbook-air-4.local>
2018-08-03 15:08:29 +0530

    Initial Commit

---
+++ p/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  p
+//
+//  Created by student14 on 05/01/01.
+//  Copyright © 2001 Felix-ITs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i,j;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;i++)
+        {
+            
+
+                printf("*\t");
+
+        }
+
+    printf("\n");
+}
+    }
